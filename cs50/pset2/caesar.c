76bf0fa2b9281e4d8875f03bd5a9580fc908b622
Jake Craige <james.craige@gmail.com>
2015-10-25 13:57:37 -0400

    Pset2 initial caesar implementation

---
+++ cs50/pset2/caesar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <cs50.h>
+
+string EncryptString(string input, int key);
+char EncryptChar(char c, int key);
+
+int main(int argc, string argv[])
+{
+    int key = atoi(argv[1]);
+    string input = GetString();
+
+    string encrypted = EncryptString(input, key);
+    printf("%s\n", encrypted);
+
+    return 0;
+}
+
+string EncryptString(string input, int key)
+{
+    char *output = malloc(strlen(input) + 1);
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        char c = input[i];
+        output[i] = isalpha(c) ?  EncryptChar(input[i], key) : c;
+    }
+
+    return output;
+}
+
+char EncryptChar(char c, int key)
+{
+    int startLetter = isupper(c) ? 'A' : 'a';
+    int encrypedCharIndex = ((c - startLetter) + key) % 26;
+
+    return startLetter + encrypedCharIndex;
+}
