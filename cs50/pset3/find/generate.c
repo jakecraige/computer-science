983799830b42b8382e05aa4b685fe8fbfe9639cb
Jake Craige <james.craige@gmail.com>
2015-10-26 18:08:29 -0400

    Comment generate.c

--- cs50/pset3/find/generate.c
+++ cs50/pset3/find/generate.c
@@ -11,7 +11,7 @@
  * where n is number of pseudorandom numbers to print
  * and s is an optional seed
  ***************************************************************************/
- 
+
 // standard libraries
 #define _XOPEN_SOURCE
 #include <cs50.h>
@@ -24,17 +24,19 @@
 
 int main(int argc, string argv[])
 {
-    // TODO: comment me
+    // Prints usage directions and exits with error if you give less than 2 or
+    // more than 3 arguments.
     if (argc != 2 && argc != 3)
     {
         printf("Usage: generate n [s]\n");
         return 1;
     }
 
-    // TODO: comment me
+    // Converts the first argument from a string to int
     int n = atoi(argv[1]);
 
-    // TODO: comment me
+    // If there is a seed argument, use it with `srand48` to set the seed that
+    // will be used when we call `drand48`
     if (argc == 3)
     {
         srand48((long int) atoi(argv[2]));
@@ -44,7 +46,7 @@ int main(int argc, string argv[])
         srand48((long int) time(NULL));
     }
 
-    // TODO: comment me
+    // Generate and print a random number using `drand48` n times
     for (int i = 0; i < n; i++)
     {
         printf("%i\n", (int) (drand48() * LIMIT));
