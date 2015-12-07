09c1e3be78d132e4f06c5eb4daee2054a3c1db39
Jake Craige <james.craige@gmail.com>
2015-12-06 19:45:37 -0800

    Implement project 4, fill and multiply in asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -8,4 +8,71 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+// set totalwords var amount of allowed "words"
+// used to determine if screen is filled yet
+@8192 // number of words for the screen I/O map (32*256)
+D=A
+@totalwords
+M=D
+
+// sets initial variables and starts listening
+@RESET
+0;JMP
+
+(LISTEN)
+  @KBD
+  D=M
+  @BLACK
+  D;JGT // if key pressed, color black
+  @WHITE
+  0;JMP // else color white
+
+// set color to black and fill
+(BLACK)
+  @color
+  M=-1
+  @FILL
+  0;JMP
+
+// set color to white and fill
+(WHITE)
+  @color
+  M=0
+  @FILL
+  0;JMP
+
+// fills @word to @color
+(FILL)
+  @word
+  D=M
+  @totalwords
+  D=M-D // D = words remaining
+  @RESET
+  D;JEQ // if words remaining == 0, screen full, reset and listen
+
+  // addr = screen + word pos
+  @word
+  D=M
+  @SCREEN
+  D=A+D
+  @addr // memory address to fill
+  M=D
+
+  // set addr to color
+  @color
+  D=M
+  @addr
+  A=M
+  M=D
+
+  @word
+  M=M+1 // word += 1
+
+  @FILL
+  0;JMP // recurse to fill whole screen
+
+(RESET)
+  @word
+  M=0
+  @LISTEN
+  0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -6,4 +6,48 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  // value = R0
+  @R0
+  D=M
+  @value
+  M=D
+
+  // multiple = R1
+  @R1
+  D=M
+  @multiple
+  M=D
+
+  // result = 0
+  @result
+  M=0
+
+(LOOP)
+  @multiple
+  D=M
+  @FINISH
+  D;JLE // if multiple <= 0; goto finish
+
+  // result += value
+  @value
+  D=M
+  @result
+  M=D+M
+
+  // multiple -= 1
+  @multiple
+  M=M-1
+
+  @LOOP
+  0;JMP
+
+(FINISH)
+  // R2 = result
+  @result
+  D=M
+  @R2
+  M=D
+
+(END)
+  @END
+  0;JMP
